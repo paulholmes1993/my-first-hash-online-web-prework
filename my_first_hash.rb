db4c0c8a8c4800494b5013531fb207b7f8d96822
Paul Holmes <paulholmes1993@gmail.com>
2019-05-13 02:01:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,12 +12,12 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3,}
   shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "muskets"=> 2, "gun powder" => 4}
 
   shipping_manifest["pearl necklace"] = 1
   shipping_manifest
